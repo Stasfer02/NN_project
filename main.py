ac62f91f36ae0ac486fa70d3298e4e10d0ea97b6
Stasfer02 <stanferguson2002@gmail.com>
2024-06-28 22:32:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pandas as pd
 from tokenizerFile import Tokenizer
+import matplotlib.pyplot as plt
+
+def plot_article_proportions(df):
+        label_counts = df['label'].value_counts()
+        labels = ['Real', 'Fake']
+        sizes = [label_counts[1], label_counts[0]]
+        colors = ['#66b3ff', '#ff9999']
+
+        plt.figure(figsize=(8, 8))
+        plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+        plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        plt.title('Proportion of Real vs Fake Articles')
+        plt.show()
 
 def main():
     
@@ -7,13 +20,17 @@ def main():
     CSV_data = pd.read_csv('cleaned_data.csv')
     #print(data.head())
 
+    plot_article_proportions(CSV_data)
+    
     # tokenize data
     myTokenizer = Tokenizer()
     test_data = CSV_data.iloc[0:4]      # test data to tokanize with (first 5 rows)
     tokenized_data:pd.DataFrame = myTokenizer.tokenize_dataframe(CSV_data)
     tokenized_data.to_csv('tokenized_data.csv', index=False)
 
-    # apply cross-validation to calculate train/test split
+
+    # perform k-fold cross validation
+    k = 10                              # NOTE number of folds; one of our hyperparameters
 
 
 
